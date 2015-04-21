ed6924d926b17993c8d501d4dfe73d70667e36c1
RossMcC <rossmcc@live.co.uk>
2015-04-21 20:23:57 +0100

    added comments for readability

--- src/main/java/com/fdmgroup/hmrcTDD/Checkout.java
+++ src/main/java/com/fdmgroup/hmrcTDD/Checkout.java
@@ -5,27 +5,31 @@ import java.util.ArrayList;
 public class Checkout {
 
 	public double calculate(ArrayList<String> shopping){
-		final double apple = 0.6;
-		final double orange = 0.25;
-		int apples= 0;
-		int oranges = 0;
-		double price = 0;
+		final double apple = 0.6;			// price of an apple
+		final double orange = 0.25;			// price of an orange
+		int apples= 0;						// number of apples scanned
+		int oranges = 0; 					// number of oranges scanned
+		double price = 0;					// the total price
 		if(shopping.isEmpty()){
 			return 0;
 		}
 		for(int i = 0; i < shopping.size(); i++){
+			// checks if there is an apple to be processed
 			if(shopping.get(i).equals("apple")){
 				price += apple;
 				apples += 1;
 				if(apples >= 2){
+					// checks if there is enough apples to merit offer
 					price -= apple;
 					apples = 0;
 				}
 			}
+			// checks if there is an orange to be processed
 			if(shopping.get(i).equals("orange")){
 				price += orange;
 				oranges += 1;
 				if(oranges >= 3){
+					// checks if there is enough oranges to merit offer
 					price -= orange;
 					oranges = 0;
 				}
